8df9cdb4bfbdab7e1253adcc1b8f64bab1798bc8
daniml3 <danimoral1001@gmail.com>
2022-01-31 10:20:11 +0530

    sm6150-common: Show a ring on the camera cutout when its in use * For avoiding halo effect on low light conditions
    * Achieve this by creating a custom ImageView that its shown when the
      camera is in use
    
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -140,6 +140,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Cutout Ring
+PRODUCT_PACKAGES += \
+    CutoutRingServiceSweet
+
 # Device Specific Settings
 PRODUCT_PACKAGES += \
     XiaomiParts
