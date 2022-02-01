3263d667fbb4e1c7bafb4cdb0c2a349a59a3be97
itz63c <itz63c@statixos.com>
2022-02-01 08:03:42 +0530

    sm6150-common: Add Notch Bar Killer overlay  * This overlay gets rid of the black bar in most apps/games    and lets the screen fill past the notch.

--- sm6150.mk
+++ sm6150.mk
@@ -320,6 +320,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosp
 
+PRODUCT_PACKAGES += \
+    NotchBarKillerSweet
+
 PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
